02795f23f9f558ab88c7405582d4c402069906ea
katerynahryshchenko <grischenkokate@gmail.com>
2023-06-16 20:40:52 +0200

    Funkcia na pridanie noveho elementa, ktora vytvori novu mnozinu do ktorej prida vsetki prvky zakladnej mnoziny a novy prvok

--- 2.c
+++ 2.c
@@ -3,8 +3,8 @@
 #include <math.h>
 
 typedef struct{
-int* data;
-int size;
+	int* data;
+	int size;
 }MNOZINA;
 
 MNOZINA* kostrukt_mnoz(){
@@ -19,26 +19,42 @@ void destruct_mnoz(MNOZINA* mnoz){
     free(mnoz);
 }
 
+void pridat(MNOZINA* mnoz, int a) {
+	MNOZINA *mnoz1;
+	int i;
+	
+	mnoz1 = kostrukt_mnoz();
+	mnoz->size = mnoz->size + 1;
+	for(i=0; i<mnoz->size; i++){
+		mnoz->data[i] = mnoz->data[i];
+	}
+	mnoz1->data[mnoz->size - 1] = a;
+	*mnoz = *mnoz1;
+	return;
+}
+
 void test(MNOZINA* mnoz, int a) {
 	int i;
-	for(i=0; i<mnoz->size; i++){
+	
+	for(i = 0; i < mnoz->size; i++){
 		if (mnoz->data[i] == a){
 		printf("Mnozina obsahuje prvok %d", a);
 		return;
 		}
 	}	
-printf("Mnozina neoobsahuje prvok %d", a);
-return;		
+	printf("Mnozina neoobsahuje prvok %d", a);
+	return;		
 }
 
 void vypis(MNOZINA* mnoz) {
 	int i;
-	for(i=0; i<mnoz->size; i++){
+	
+	for(i=0; i < mnoz->size; i++){
 		printf("%d ", mnoz->data[i]);
 	}
 }
 
 int main() {
 
-return 0;
+	return 0;
 }
